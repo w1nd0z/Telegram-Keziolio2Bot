a4d73de21b54231c12cbcb7eaeeb658c15bda0bd
Keziolio <keziolio123@gmail.com>
2017-02-25 23:02:07 +0100

    ban fucking deerf

--- bot.js
+++ bot.js
@@ -208,6 +208,17 @@ bot.registerAdminCmd("!cc", (mess) => {
 
 bot.onUpdate(function (up) {
 
+    try{
+    	//ban deerf
+    	if(up.message.from.id==257413173){
+    		bot.send('kickChatMember', {
+          			chat_id: ip.message.chat.id,
+          			user_id: 257413173,
+        		});
+
+    	}
+    }catch(e){}
+
     /*
      * useless welcome message
      */
